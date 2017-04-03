f2db1a17e8d88f142bb601d3c2eaa22a7a4fdaf2
Maciej Leszczyński <makegraff@op.pl>
2017-04-03 13:18:04 +0200

    Work on contact section

--- script.js
+++ script.js
@@ -306,42 +306,41 @@ const initPhotoSwipeFromDOM = function (gallerySelector) {
 initPhotoSwipeFromDOM('.js-gallery')
 
 const scrollToSection = (
-    href,
-    margin = 0,
-    dontChangeUrl = false,
-    duration = 600
+  href,
+  margin = 0,
+  dontChangeUrl = false,
+  duration = 600
 ) => {
+  const scrollToTop = href === '#'
+  const shift = scrollY < 300 ? 300 : 0
+  let position = 0
 
-    const scrollToTop = href === '#'
-    let position = 0
+  if (!dontChangeUrl) {
+    let path = scrollToTop
+      ? (window.location.pathname || '/') + window.location.search
+      : href
 
-    if (!dontChangeUrl) {
-        let path = scrollToTop
-            ? (window.location.pathname || '/') + window.location.search
-            : href
+    history.pushState(null, null, path)
+  }
 
-        history.pushState(null, null, path)
-    }
+  if (!scrollToTop) {
+    let section = document.querySelector(href)
 
-    if (!scrollToTop) {
-        let section = document.querySelector(href)
+    if (!section) return
 
-        if (!section) return
+    position = section.getBoundingClientRect().top + scrollY - margin + shift
+  }
 
-        position = section.getBoundingClientRect().top + scrollY - margin
-    }
+  const start = Date.now()
+  const scroll = () => {
+    let step = Math.min(1, (Date.now() - start) / duration)
+    let distance = position - scrollY
 
-    const start = Date.now()
-    const scroll = () => {
-        let step = Math.min(1, (Date.now() - start) / duration)
-        let distance = position - scrollY
+    window.scrollBy(0, Math.round(step * distance))
 
-        window.scrollBy(0, Math.round(step * distance))
-
-        ;(step !== 1) && requestAnimationFrame(scroll)
-    }
-    requestAnimationFrame(scroll)
-
+    ;(step !== 1) && requestAnimationFrame(scroll)
+  }
+  requestAnimationFrame(scroll)
 }
 
 ;[].forEach.call(
@@ -354,3 +353,22 @@ const scrollToSection = (
     })
   }
 )
+
+function initMap() {
+  const bp = matchMedia('(min-width: 900px)').matches
+  const markerPosition = { lat: 50.66939, lng: 17.9550803 }
+  const shiftedPosition = { lat: 50.668404, lng: 17.9534495 }
+
+  const map = new google.maps.Map(document.querySelector('.js-map'), {
+    center: bp ? shiftedPosition : markerPosition,
+    zoom: 16,
+    disableDefaultUI: true,
+    scrollwheel: false,
+    styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
+  })
+
+  new google.maps.Marker({
+    position: markerPosition,
+    map
+  });
+}
